714e287dbd1667f01daa61cecaac8bbdffad9d5c
Prajwal Koirala <16564273+Prajwal-Koirala@users.noreply.github.com>
2021-01-29 20:58:44 -0500

    Update main.go

--- main.go
+++ main.go
@@ -1,4 +1,20 @@
-pacakge main
+package main
 
-import "fmt"
+import (
+    "fmt"
+    "runtime"
+)
 
+func main() {
+    os := runtime.GOOS
+    switch os {
+    case "windows":
+        fmt.Println("Windows")
+    case "darwin":
+        fmt.Println("MAC operating system")
+    case "linux":
+        fmt.Println("Linux")
+    default:
+        fmt.Printf("%s.\n", os)
+    }
+}
