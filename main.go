6d354b6ef1f1ef1cb8dafa8742779cbad00a4965
Prajwal Koirala <16564273+Prajwal-Koirala@users.noreply.github.com>
2021-01-29 21:13:30 -0500

    Update main.go

--- main.go
+++ main.go
@@ -1,29 +1,45 @@
 package main
 
 import (
-    "fmt"
-    "runtime"
+	"fmt"
+	"log"
+	"os"
+	"runtime"
 )
 
 func main() {
-    selectOperatingSystem()
+	selectOperatingSystem()
 }
 
 func selectOperatingSystem() {
-    os := runtime.GOOS
-    switch os {
-    case "windows":
-        installFlutterOnWindows()
-    case "darwin":
-        fmt.Println("MAC operating system")
-    case "linux":
-        fmt.Println("Linux")
-    default:
-        fmt.Printf("Error: %s Not Supported.\n", os)
-    }
+	os := runtime.GOOS
+	switch os {
+	case "windows":
+		installFlutterOnWindows()
+	case "darwin":
+		fmt.Println("MAC operating system")
+	case "linux":
+		fmt.Println("Linux")
+	default:
+		fmt.Printf("Error: %s Not Supported.\n", os)
+	}
 }
 
 func installFlutterOnWindows() {
-    os.Mkdir("/src/", 0755)
-    //os.Chdir("/src")
+	windowsDir := "/src"
+	if isNotExist(windowsDir) {
+		os.Mkdir(windowsDir, 0755)
+		//os.Chdir("/src")
+	} else {
+		log.Println("Error: Couldn't create project.")
+		os.Exit(0)
+	}
+}
+
+func isNotExist(filename string) bool {
+	info, err := os.Stat(filename)
+	if os.IsNotExist(err) {
+		return true
+	}
+	return !info.IsDir()
 }
