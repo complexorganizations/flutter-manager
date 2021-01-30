135a638ed2d4d5c09c342f162df2b8705132df7f
Prajwal Koirala <16564273+Prajwal-Koirala@users.noreply.github.com>
2021-01-30 07:43:15 -0500

    Update main.go

--- main.go
+++ main.go
@@ -14,6 +14,7 @@ func main() {
 func flutterInstalledCheck() {
 	if commandExists("flutter") {
 		log.Println("Error: Flutter found on the system")
+		os.Exit(0)
         } else {
 		gitInstalledCheck()
         }
@@ -24,7 +25,7 @@ func gitInstalledCheck() {
 		selectOperatingSystem()
         } else {
 		log.Println("Error: Git not found on the system")
-		os.Exit(1)
+		os.Exit(0)
         }
 }
 
