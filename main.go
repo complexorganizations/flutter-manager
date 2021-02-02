2e7e6d48d2414b81114989d9eaf20187c3e50c55
Prajwal Koirala <16564273+Prajwal-Koirala@users.noreply.github.com>
2021-02-01 21:28:02 -0500

    Update main.go

--- main.go
+++ main.go
@@ -6,6 +6,7 @@ import (
 	"os"
 	"os/exec"
 	"runtime"
+	"strings"
 )
 
 func main() {
@@ -37,6 +38,15 @@ func commandsRequirementsCheck() {
 	}
 	if !commandExists("git") {
 		log.Println("Error: Git was not discovered in the system.")
+		os.Exit(0)
+	}
+}
+
+func checkSystemPath() {
+	data, err := ioutil.ReadAll(os.Environ())
+	fileData := string(data)
+	if strings.Contains(fileData, "flutter") {
+		log.Println("Error: Flutter was discovered in the system.")
 		os.Exit(0)
 	}
 }
